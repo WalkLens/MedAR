6abd2cfdea8bab71f7a792a3efc95430c4954ff6
IXA <72248342+kgy6022@users.noreply.github.com>
2024-02-24 21:48:45 +0900

    fix: refactor LunarcomAgent.cs, make a text converter prefab

--- MedAR/Assets/Lunarcom_Agent.cs
+++ MedAR/Assets/Lunarcom_Agent.cs
@@ -7,29 +7,7 @@ using TMPro;
 public class Lunarcom_Agent : Socket_Agent
 {
     public TextMeshProUGUI returnData;
-    //public string returnData;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //requestData = "Hi from Unity!";
-        //responseData = ServerRequest(requestData);
-
-        //Debug.Log(responseData);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if (returnData.text.Length >= 1)
-        //{
-        //    Debug.Log(returnData);
-        //}
-        //else
-        //{
-        //    Debug.LogError("text is null");
-        //}
-    }
-
+   
     public void ReturnServerRequest(string requestData)
     {
         returnData.text = ServerRequest(requestData);
